887cf4d81211afbe9c802dad16cd85d1da579f5b
Steve Robbins <steve@steverobbins.com>
2016-01-14 11:06:15 -0800

    Adding test for prompts during "admin:user:create" command

--- setup/src/Magento/Setup/Test/Unit/Console/Command/AdminUserCreateCommandTest.php
+++ setup/src/Magento/Setup/Test/Unit/Console/Command/AdminUserCreateCommandTest.php
@@ -9,6 +9,8 @@ use Magento\Setup\Model\AdminAccount;
 use Magento\Setup\Console\Command\AdminUserCreateCommand;
 use Magento\Setup\Mvc\Bootstrap\InitParamListener;
 use Magento\User\Model\UserValidationRules;
+use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Helper\DialogHelper;
 use Symfony\Component\Console\Tester\CommandTester;
 
 class AdminUserCreateCommandTest extends \PHPUnit_Framework_TestCase
@@ -27,6 +29,7 @@ class AdminUserCreateCommandTest extends \PHPUnit_Framework_TestCase
     {
         $this->installerFactoryMock = $this->getMock(\Magento\Setup\Model\InstallerFactory::class, [], [], '', false);
         $this->command = new AdminUserCreateCommand($this->installerFactoryMock, new UserValidationRules());
+        $this->command->setHelperSet(new HelperSet([new DialogHelper]));
     }
 
     public function testExecute()
@@ -44,7 +47,6 @@ class AdminUserCreateCommandTest extends \PHPUnit_Framework_TestCase
             AdminAccount::KEY_EMAIL => 'test@test.com',
             AdminAccount::KEY_FIRST_NAME => 'John',
             AdminAccount::KEY_LAST_NAME => 'Doe',
-            InitParamListener::BOOTSTRAP_PARAM => null,
         ];
         $commandTester = new CommandTester($this->command);
         $installerMock = $this->getMock(\Magento\Setup\Model\Installer::class, [], [], '', false);
@@ -52,6 +54,27 @@ class AdminUserCreateCommandTest extends \PHPUnit_Framework_TestCase
         $this->installerFactoryMock->expects($this->once())->method('create')->willReturn($installerMock);
         $commandTester->execute($options);
         $this->assertEquals('Created Magento administrator user named user' . PHP_EOL, $commandTester->getDisplay());
+    }
+
+    public function testExecuteWithPrompts()
+    {
+        $data = [
+            AdminAccount::KEY_USER => 'user',
+            AdminAccount::KEY_PASSWORD => '123123q',
+            AdminAccount::KEY_EMAIL => 'test@test.com',
+            AdminAccount::KEY_FIRST_NAME => 'John',
+            AdminAccount::KEY_LAST_NAME => 'Doe',
+        ];
+        $commandTester = new CommandTester($this->command);
+        $installerMock = $this->getMock('Magento\Setup\Model\Installer', [], [], '', false);
+        $installerMock->expects($this->once())->method('installAdminUser')->with($data);
+        $this->installerFactoryMock->expects($this->once())->method('create')->willReturn($installerMock);
+
+        $helper = $this->command->getHelper('dialog');
+        $helper->setInputStream($this->getInputStream("John\nDoe\nuser\ntest@test.com\n123123q\n"));
+
+        $commandTester->execute([]);
+        $this->assertContains('Created Magento administrator user named user', $commandTester->getDisplay());
     }
 
     public function testGetOptionsList()
@@ -68,17 +91,7 @@ class AdminUserCreateCommandTest extends \PHPUnit_Framework_TestCase
      */
     public function testValidate(array $options, array $errors)
     {
-        $inputMock = $this->getMockForAbstractClass(
-            \Symfony\Component\Console\Input\InputInterface::class,
-            [],
-            '',
-            false
-        );
-        $index = 0;
-        foreach ($options as $option) {
-            $inputMock->expects($this->at($index++))->method('getOption')->willReturn($option);
-        }
-        $this->assertEquals($errors, $this->command->validate($inputMock));
+        $this->assertEquals($errors, $this->command->validate($options));
     }
 
     /**
@@ -87,18 +100,54 @@ class AdminUserCreateCommandTest extends \PHPUnit_Framework_TestCase
     public function validateDataProvider()
     {
         return [
-            [[null, 'Doe', 'admin', 'test@test.com', '123123q', '123123q'], ['First Name is a required field.']],
             [
-                ['John', null, null, 'test@test.com', '123123q', '123123q'],
+                [
+                    AdminAccount::KEY_FIRST_NAME => null,
+                    AdminAccount::KEY_LAST_NAME  => 'Doe',
+                    AdminAccount::KEY_USER       => 'admin',
+                    AdminAccount::KEY_EMAIL      => 'test@test.com',
+                    AdminAccount::KEY_PASSWORD   => '123123q',
+                ],
+                ['First Name is a required field.']
+            ],
+            [
+                [
+                    AdminAccount::KEY_FIRST_NAME => 'John',
+                    AdminAccount::KEY_LAST_NAME  => null,
+                    AdminAccount::KEY_USER       => null,
+                    AdminAccount::KEY_EMAIL      => 'test@test.com',
+                    AdminAccount::KEY_PASSWORD   => '123123q',
+                ],
                 ['User Name is a required field.', 'Last Name is a required field.'],
             ],
-            [['John', 'Doe', 'admin', null, '123123q', '123123q'], ['Please enter a valid email.']],
             [
-                ['John', 'Doe', 'admin', 'test', '123123q', '123123q'],
+                [
+                    AdminAccount::KEY_FIRST_NAME => 'John',
+                    AdminAccount::KEY_LAST_NAME  => 'Doe',
+                    AdminAccount::KEY_USER       => 'admin',
+                    AdminAccount::KEY_EMAIL      => null,
+                    AdminAccount::KEY_PASSWORD   => '123123q',
+                ],
+                ['Please enter a valid email.']
+            ],
+            [
+                [
+                    AdminAccount::KEY_FIRST_NAME => 'John',
+                    AdminAccount::KEY_LAST_NAME  => 'Doe',
+                    AdminAccount::KEY_USER       => 'admin',
+                    AdminAccount::KEY_EMAIL      => 'test',
+                    AdminAccount::KEY_PASSWORD   => '123123q',
+                ],
                 ["'test' is not a valid email address in the basic format local-part@hostname"]
             ],
             [
-                ['John', 'Doe', 'admin', 'test@test.com', '', ''],
+                [
+                    AdminAccount::KEY_FIRST_NAME => 'John',
+                    AdminAccount::KEY_LAST_NAME  => 'Doe',
+                    AdminAccount::KEY_USER       => 'admin',
+                    AdminAccount::KEY_EMAIL      => 'test@test.com',
+                    AdminAccount::KEY_PASSWORD   => '', 
+            ],
                 [
                     'Password is required field.',
                     'Your password must be at least 7 characters.',
@@ -106,17 +155,47 @@ class AdminUserCreateCommandTest extends \PHPUnit_Framework_TestCase
                 ]
             ],
             [
-                ['John', 'Doe', 'admin', 'test@test.com', '123123', '123123'],
+                [
+                    AdminAccount::KEY_FIRST_NAME => 'John',
+                    AdminAccount::KEY_LAST_NAME  => 'Doe',
+                    AdminAccount::KEY_USER       => 'admin',
+                    AdminAccount::KEY_EMAIL      => 'test@test.com',
+                    AdminAccount::KEY_PASSWORD   => '123123',
+                ],
                 [
                     'Your password must be at least 7 characters.',
                     'Your password must include both numeric and alphabetic characters.'
                 ]
             ],
             [
-                ['John', 'Doe', 'admin', 'test@test.com', '1231231', '1231231'],
+                [
+                    AdminAccount::KEY_FIRST_NAME => 'John',
+                    AdminAccount::KEY_LAST_NAME  => 'Doe',
+                    AdminAccount::KEY_USER       => 'admin',
+                    AdminAccount::KEY_EMAIL      => 'test@test.com',
+                    AdminAccount::KEY_PASSWORD   => '1231231',
+                ],
                 ['Your password must include both numeric and alphabetic characters.']
             ],
-            [['John', 'Doe', 'admin', 'test@test.com', '123123q', '123123q'], []],
+            [
+                [
+                    AdminAccount::KEY_FIRST_NAME => 'John',
+                    AdminAccount::KEY_LAST_NAME  => 'Doe',
+                    AdminAccount::KEY_USER       => 'admin',
+                    AdminAccount::KEY_EMAIL      => 'test@test.com',
+                    AdminAccount::KEY_PASSWORD   => '123123q',
+                ],
+                []
+            ],
         ];
+    }
+
+    protected function getInputStream($input)
+    {
+        $stream = fopen('php://memory', 'r+', false);
+        fputs($stream, $input);
+        rewind($stream);
+
+        return $stream;
     }
 }
