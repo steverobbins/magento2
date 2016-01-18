6268d267646d21382af3421870fca77f18e02e09
Steve Robbins <steve@steverobbins.com>
2016-01-18 12:08:23 -0800

    MAGETWO-47940: Reworking admin:user:create command logic
    - Reverting change to signature of validate method
    - Addressing buskamuza's comment on accessing unset keys in array
    - Created Magento specific input type with prompt option (needed to access protected $definition)

--- setup/src/Magento/Setup/Test/Unit/Console/Command/AdminUserCreateCommandTest.php
+++ setup/src/Magento/Setup/Test/Unit/Console/Command/AdminUserCreateCommandTest.php
@@ -5,6 +5,7 @@
  */
 namespace Magento\Setup\Test\Unit\Console\Command;
 
+use Magento\Framework\Console\Input;
 use Magento\Setup\Model\AdminAccount;
 use Magento\Setup\Console\Command\AdminUserCreateCommand;
 use Magento\Setup\Mvc\Bootstrap\InitParamListener;
@@ -47,6 +48,7 @@ class AdminUserCreateCommandTest extends \PHPUnit_Framework_TestCase
             AdminAccount::KEY_EMAIL => 'test@test.com',
             AdminAccount::KEY_FIRST_NAME => 'John',
             AdminAccount::KEY_LAST_NAME => 'Doe',
+            InitParamListener::BOOTSTRAP_PARAM => null,
         ];
         $commandTester = new CommandTester($this->command);
         $installerMock = $this->getMock(\Magento\Setup\Model\Installer::class, [], [], '', false);
@@ -54,6 +56,67 @@ class AdminUserCreateCommandTest extends \PHPUnit_Framework_TestCase
         $this->installerFactoryMock->expects($this->once())->method('create')->willReturn($installerMock);
         $commandTester->execute($options);
         $this->assertEquals('Created Magento administrator user named user' . PHP_EOL, $commandTester->getDisplay());
+    }
+
+    public function testGetOptionsList()
+    {
+        /* @var $argsList \Symfony\Component\Console\Input\InputArgument[] */
+        $argsList = $this->command->getOptionsList();
+        $this->assertEquals(AdminAccount::KEY_EMAIL, $argsList[2]->getName());
+    }
+
+    /**
+     * @dataProvider validateDataProvider
+     * @param bool[] $options
+     * @param string[] $errors
+     */
+    public function testValidate(array $options, array $errors)
+    {
+        $inputMock = $this->getMockForAbstractClass('Symfony\Component\Console\Input\InputInterface', [], '', false);
+        $index = 0;
+        foreach ($options as $option) {
+            $inputMock->expects($this->at($index++))->method('getOption')->willReturn($option);
+        }
+        $this->assertEquals($errors, $this->command->validate($inputMock));
+    }
+
+    /**
+     * @return array
+     */
+    public function validateDataProvider()
+    {
+        return [
+            [[null, 'Doe', 'admin', 'test@test.com', '123123q', '123123q'], ['First Name is a required field.']],
+            [
+                ['John', null, null, 'test@test.com', '123123q', '123123q'],
+                ['User Name is a required field.', 'Last Name is a required field.'],
+            ],
+            [['John', 'Doe', 'admin', null, '123123q', '123123q'], ['Please enter a valid email.']],
+            [
+                ['John', 'Doe', 'admin', 'test', '123123q', '123123q'],
+                ["'test' is not a valid email address in the basic format local-part@hostname"]
+            ],
+            [
+                ['John', 'Doe', 'admin', 'test@test.com', '', ''],
+                [
+                    'Password is required field.',
+                    'Your password must be at least 7 characters.',
+                    'Your password must include both numeric and alphabetic characters.'
+                ]
+            ],
+            [
+                ['John', 'Doe', 'admin', 'test@test.com', '123123', '123123'],
+                [
+                    'Your password must be at least 7 characters.',
+                    'Your password must include both numeric and alphabetic characters.'
+                ]
+            ],
+            [
+                ['John', 'Doe', 'admin', 'test@test.com', '1231231', '1231231'],
+                ['Your password must include both numeric and alphabetic characters.']
+            ],
+            [['John', 'Doe', 'admin', 'test@test.com', '123123q', '123123q'], []],
+        ];
     }
 
     public function testExecuteWithPrompts()
@@ -73,121 +136,8 @@ class AdminUserCreateCommandTest extends \PHPUnit_Framework_TestCase
         $helper = $this->command->getHelper('dialog');
         $helper->setInputStream($this->getInputStream("John\nDoe\nuser\ntest@test.com\n123123q\n"));
 
-        $commandTester->execute([]);
+        $commandTester->execute([], ['interactive' => new Input]);
         $this->assertContains('Created Magento administrator user named user', $commandTester->getDisplay());
-    }
-
-    public function testGetOptionsList()
-    {
-        /* @var $argsList \Symfony\Component\Console\Input\InputArgument[] */
-        $argsList = $this->command->getOptionsList();
-        $this->assertEquals(AdminAccount::KEY_EMAIL, $argsList[2]->getName());
-    }
-
-    /**
-     * @dataProvider validateDataProvider
-     * @param bool[] $options
-     * @param string[] $errors
-     */
-    public function testValidate(array $options, array $errors)
-    {
-        $this->assertEquals($errors, $this->command->validate($options));
-    }
-
-    /**
-     * @return array
-     */
-    public function validateDataProvider()
-    {
-        return [
-            [
-                [
-                    AdminAccount::KEY_FIRST_NAME => null,
-                    AdminAccount::KEY_LAST_NAME  => 'Doe',
-                    AdminAccount::KEY_USER       => 'admin',
-                    AdminAccount::KEY_EMAIL      => 'test@test.com',
-                    AdminAccount::KEY_PASSWORD   => '123123q',
-                ],
-                ['First Name is a required field.']
-            ],
-            [
-                [
-                    AdminAccount::KEY_FIRST_NAME => 'John',
-                    AdminAccount::KEY_LAST_NAME  => null,
-                    AdminAccount::KEY_USER       => null,
-                    AdminAccount::KEY_EMAIL      => 'test@test.com',
-                    AdminAccount::KEY_PASSWORD   => '123123q',
-                ],
-                ['User Name is a required field.', 'Last Name is a required field.'],
-            ],
-            [
-                [
-                    AdminAccount::KEY_FIRST_NAME => 'John',
-                    AdminAccount::KEY_LAST_NAME  => 'Doe',
-                    AdminAccount::KEY_USER       => 'admin',
-                    AdminAccount::KEY_EMAIL      => null,
-                    AdminAccount::KEY_PASSWORD   => '123123q',
-                ],
-                ['Please enter a valid email.']
-            ],
-            [
-                [
-                    AdminAccount::KEY_FIRST_NAME => 'John',
-                    AdminAccount::KEY_LAST_NAME  => 'Doe',
-                    AdminAccount::KEY_USER       => 'admin',
-                    AdminAccount::KEY_EMAIL      => 'test',
-                    AdminAccount::KEY_PASSWORD   => '123123q',
-                ],
-                ["'test' is not a valid email address in the basic format local-part@hostname"]
-            ],
-            [
-                [
-                    AdminAccount::KEY_FIRST_NAME => 'John',
-                    AdminAccount::KEY_LAST_NAME  => 'Doe',
-                    AdminAccount::KEY_USER       => 'admin',
-                    AdminAccount::KEY_EMAIL      => 'test@test.com',
-                    AdminAccount::KEY_PASSWORD   => '', 
-            ],
-                [
-                    'Password is required field.',
-                    'Your password must be at least 7 characters.',
-                    'Your password must include both numeric and alphabetic characters.'
-                ]
-            ],
-            [
-                [
-                    AdminAccount::KEY_FIRST_NAME => 'John',
-                    AdminAccount::KEY_LAST_NAME  => 'Doe',
-                    AdminAccount::KEY_USER       => 'admin',
-                    AdminAccount::KEY_EMAIL      => 'test@test.com',
-                    AdminAccount::KEY_PASSWORD   => '123123',
-                ],
-                [
-                    'Your password must be at least 7 characters.',
-                    'Your password must include both numeric and alphabetic characters.'
-                ]
-            ],
-            [
-                [
-                    AdminAccount::KEY_FIRST_NAME => 'John',
-                    AdminAccount::KEY_LAST_NAME  => 'Doe',
-                    AdminAccount::KEY_USER       => 'admin',
-                    AdminAccount::KEY_EMAIL      => 'test@test.com',
-                    AdminAccount::KEY_PASSWORD   => '1231231',
-                ],
-                ['Your password must include both numeric and alphabetic characters.']
-            ],
-            [
-                [
-                    AdminAccount::KEY_FIRST_NAME => 'John',
-                    AdminAccount::KEY_LAST_NAME  => 'Doe',
-                    AdminAccount::KEY_USER       => 'admin',
-                    AdminAccount::KEY_EMAIL      => 'test@test.com',
-                    AdminAccount::KEY_PASSWORD   => '123123q',
-                ],
-                []
-            ],
-        ];
     }
 
     protected function getInputStream($input)
